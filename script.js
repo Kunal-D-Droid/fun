a0f0d509776e0f69a68c16a9122b62c08b9ec835
Kunal Das <55740289+Kunal-D-Droid@users.noreply.github.com>
2023-04-24 02:54:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+Government Shutdown in pure CSS by Joe Rezendes
+
+You may need to switch the editor view to the side to see this as intended
+
+Want to see more? Follow me on github:
+https://github.com/joerez
+
+Portfolio Website:
+https://www.joerezendes.com
+
+Note: While this creation is political, it was made as a satire and is not nesessarily reflective of my own personal political views.
+
+*/
